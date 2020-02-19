e21b0167e3880a406aa0ab6742bec194b0a85919
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2020-02-19 11:35:13 +0530

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi people,</h1>
+    <p>Welcome to <bold>English Classes Live</bold>.</p>
+    <p>We'll be live soon...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
